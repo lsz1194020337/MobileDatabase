ecdf7138e17f73a317d19acab950c684fdab50a8
l1194 <17803989103@163.com>
2022-06-29 05:15:32 +0800

    finish all pages except the update data page

--- app/src/main/java/com/example/mobiledatabase/activity/DatabaseInfoActivity.java
+++ app/src/main/java/com/example/mobiledatabase/activity/DatabaseInfoActivity.java
@@ -1,15 +1,21 @@
 package com.example.mobiledatabase.activity;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mobiledatabase.R;
@@ -19,7 +25,7 @@ import com.example.mobiledatabase.utils.MySQLiteHelper;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DatabaseInfoActivity extends AppCompatActivity {
+public class DatabaseInfoActivity extends AppCompatActivity implements AdapterView.OnItemLongClickListener {
     private Intent intent;
     private String databaseName;
     private TextView fileName;
@@ -28,6 +34,7 @@ public class DatabaseInfoActivity extends AppCompatActivity {
     private ListView listView;
     private List<String> tableList;
     private DatabaseAdapter adapter;
+    private String sql;
 
 
     @Override
@@ -46,7 +53,55 @@ public class DatabaseInfoActivity extends AppCompatActivity {
         tableList = getTableList(databaseName);
         adapter = new DatabaseAdapter(this, tableList);
         listView.setAdapter(adapter);
+        listView.setOnItemLongClickListener(this);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(DatabaseInfoActivity.this, "Welcome to Table " + tableList.get(position), Toast.LENGTH_SHORT).show();
+                //deliver the db file to the second page
+                Intent intent = new Intent(DatabaseInfoActivity.this, UpdateDataActivity.class);
+                intent.putExtra("databaseName", tableList.get(position));
+                intent.putExtra("tableName", tableList.get(position));
+                startActivity(intent);
+            }
+        });
+    }
 
+    //DBList button
+    public void jumpToMainPage(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    //New button
+    public void jumpToCreateDBPage(View view) {
+        startActivity(new Intent(this, CreateDatabaseActivity.class));
+    }
+
+    //NewTable button
+    public void jumpToCreateTablePage(View view) {
+        Intent intent = new Intent(DatabaseInfoActivity.this, CreateTableActivity.class);
+        intent.putExtra("databaseName", databaseName);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1: {
+                //flash page
+                if (resultCode == 1) {
+                    databaseName = data.getStringExtra("databaseName");
+                    tableList = getTableList(databaseName);
+                    fileName.setText(databaseName);
+                    adapter = new DatabaseAdapter(this, this.tableList);
+                    listView.setAdapter(adapter);
+                }
+                break;
+            }
+            default:
+                break;
+        }
     }
 
     @SuppressLint("Range")
@@ -65,40 +120,66 @@ public class DatabaseInfoActivity extends AppCompatActivity {
         return tableList;
     }
 
-    //DBList button
-    public void jumpToMainPage(View view) {
-        startActivity(new Intent(this, MainActivity.class));
-    }
-
-    //New button
-    public void jumpToCreateDBPage(View view) {
-        startActivity(new Intent(this, CreateDatabaseActivity.class));
-    }
-
-    //NewTable button
-    public void jumpToCreateTablePage(View view) {
-        Intent intent = new Intent(DatabaseInfoActivity.this, CreateTableActivity.class);
-        intent.putExtra("databaseName", databaseName);
-        startActivityForResult(intent,1);
-    }
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case 1:{
-                //flash page
-                if(resultCode==1){
-                    databaseName = data.getStringExtra("databaseName");
-                    tableList = getTableList(databaseName);
-                    fileName.setText(databaseName);
-                    adapter = new DatabaseAdapter(this, this.tableList);
-                    listView.setAdapter(adapter);
-                }
-                break;
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        //delete dialog
+        //create dialog builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //create dialog
+        AlertDialog dialog = builder.create();
+        View dialogView = View.inflate(this, R.layout.delete_dialog, null);
+        TextView tv_name = dialogView.findViewById(R.id.tv_dialogName);
+        TextView tv_delete = dialogView.findViewById(R.id.tv_delete);
+
+        //delete
+        tv_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteItem(position);
+                dialog.dismiss();
+                //delete the table
+                mySQLiteHelper = new MySQLiteHelper(DatabaseInfoActivity.this, databaseName, null, 1);
+                db = mySQLiteHelper.getWritableDatabase();
+                sql = "drop table " + tableList.get(position) + ";";
+                db.execSQL(sql);
             }
-            default:
-                break;
-        }
+        });
+        tv_name.setText(tableList.get(position));
+        dialog.setView(dialogView);
+        dialog.show();
+
+        //change the size of dialog must put behind the dialog.show()
+        //get params of the dialog of the hole page
+        WindowManager.LayoutParams params = dialog.getWindow().getAttributes();
+        WindowManager windowManager = getWindowManager();
+        Point p = new Point();
+        windowManager.getDefaultDisplay().getSize(p);
+        params.width = (p.x) / 3;
+        dialog.getWindow().setAttributes(params);
+        return true;
     }
+
+    private void deleteItem(int position) {
+        //create dialog builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete");
+        builder.setMessage("Are you sure to delete this table ? ");
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //delete item need to delete the file
+                tableList.remove(position);
+                listView.setAdapter(adapter);
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        builder.show();
+    }
+
+
 }
--- app/src/main/java/com/example/mobiledatabase/activity/UpdateDataActivity.java
+++ app/src/main/java/com/example/mobiledatabase/activity/UpdateDataActivity.java
@@ -3,17 +3,28 @@ package com.example.mobiledatabase.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mobiledatabase.R;
 
 public class UpdateDataActivity extends AppCompatActivity {
+    private Intent intent;
+    private String databaseName;
+    private String tableName;
+    private TextView table;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.update_table);
+        setContentView(R.layout.update_data);
+
+        intent = getIntent();
+        databaseName = intent.getStringExtra("databaseName");
+        tableName = intent.getStringExtra("tableName");
+        table = findViewById(R.id.textView4);
+        table.setText("Table: " + tableName);
     }
 
     //create or delete table
